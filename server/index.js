9503abd2a8bdeb09dff141ee23b1553723764700
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-28 07:50:35 +0200

    Allow CORS

--- server/index.js
+++ server/index.js
@@ -4,3 +4,10 @@ const express = require('express');
 // Set port, init express
 const port = process.env.PORT || 8080;
 const app = express();
+
+//Allows CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});

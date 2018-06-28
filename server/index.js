7e96bac5b19d14dd9858441ab696a1d26508e7fd
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-28 08:32:29 +0200

    Init Bunyan

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bunyan = require('bunyan');
+const bunyanConfig = require('./configs/bunyan');
 
 // Set port, init express
 const port = process.env.PORT || 8080;
@@ -11,3 +13,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+// Init bunyan logger
+const log = bunyan.createLogger(bunyanConfig);

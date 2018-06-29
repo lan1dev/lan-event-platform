6e838e12e1fe2afa3cd14e689bad02b6615d3e1f
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 15:01:16 +0200

    Add sidebar config

---
+++ client/configs/sidebarConfig.js
@@ -0,0 +1,16 @@
+const sidebarConfig = [
+  {
+    name: 'Home',
+    href: '/dashboard'
+  },
+  {
+    name: 'Tickets',
+    href: '/dashboard/tickets'
+  },
+  {
+    name: 'Logout',
+    href: '/logout'
+  }
+];
+
+export default sidebarConfig;

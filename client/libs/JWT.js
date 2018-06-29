e4879ef4c215566d38b9972dc7df1cd620eeb241
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 15:15:05 +0200

    Add JWT lib

---
+++ client/libs/JWT.js
@@ -0,0 +1,14 @@
+import JwtDecode from 'jwt-decode';
+
+class Jwt {
+  static decode(token) {
+    const decoded = JwtDecode(token);
+    return decoded;
+  }
+
+  static getUsername(token) {
+    return this.decode(token).username;
+  }
+}
+
+export default Jwt;

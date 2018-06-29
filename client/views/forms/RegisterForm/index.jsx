f37864ae86eb9b79dfdc78dfe7ab7546a4ed0165
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 14:33:57 +0200

    Add RegisterForm component

---
+++ client/views/forms/RegisterForm/index.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import InputField from '../../common/InputField';
+import Button from '../../common/Button';
+import Form from '../../common/Form';
+import Text from '../../common/Text';
+import { BASE_URI } from '../../../configs/URI';
+
+const defaultProps = {
+  errors: []
+};
+
+const propTypes = {
+  sendForm: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  success: PropTypes.bool.isRequired,
+  errors: PropTypes.arrayOf(PropTypes.string)
+};
+
+/**
+ * RegisterForm Component
+ */
+class RegisterForm extends Component {
+  handleSubmit = values => {
+    /* eslint-disable-next-line react/destructuring-assignment */
+    this.props.sendForm(`${BASE_URI}/auth/signup`, values);
+  };
+
+  render() {
+    const { handleSubmit, errors, success } = this.props;
+    return (
+      <Form onSubmit={handleSubmit(this.handleSubmit)}>
+        <Field name="name" label="&#xf003; Name" component={InputField} type="text" />
+        <Field name="email" label="&#xf003; Email" component={InputField} type="email" />
+        <Field name="password" label="&#xf13e;  Password" component={InputField} type="password" />
+        {errors.map(errorMessage => <Text error>{errorMessage}</Text>)}
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
+          onChange={this.handleChange}
+        >
+          Register
+        </Button>
+        {success && <Redirect to="/" />}
+      </Form>
+    );
+  }
+}
+
+RegisterForm.defaultProps = defaultProps;
+RegisterForm.propTypes = propTypes;
+
+/* eslint-disable no-class-assign */
+RegisterForm = reduxForm({
+  form: 'register'
+})(RegisterForm);
+
+export default RegisterForm;

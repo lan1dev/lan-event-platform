e213f51e825c5b06c50ec69e3f8dab46fff6750a
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 14:18:11 +0200

    Add Homepage Page component

---
+++ client/views/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HomePageBase from './HomePageBase';
+
+/**
+ * HomePage for the root (/) route
+ */
+const HomePage = () => <HomePageBase>Sections are missing...</HomePageBase>;
+
+export default HomePage;

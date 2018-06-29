94eaa5233ea492799ce5c2555230486625bf6b6c
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 15:09:59 +0200

    DashboardHome Page

---
+++ client/views/pages/DashboardHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import DashboardBase from './DashboardBase';
+
+/**
+ * DashboardPage for the /dashboard route
+ */
+const TicketsPage = () => <DashboardBase>Welcome</DashboardBase>;
+
+export default TicketsPage;

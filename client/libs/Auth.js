4efe2833a32009fd6b11fca9323668908589a718
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 15:14:36 +0200

    Add Auth lib

---
+++ client/libs/Auth.js
@@ -0,0 +1,25 @@
+import JWT from './JWT';
+
+class Auth {
+  static authenticateUser(token) {
+    localStorage.setItem('token', token);
+  }
+
+  static isUserAuthenticated() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      return JWT.decode(token);
+    }
+    return token;
+  }
+
+  static deauthenticateUser() {
+    localStorage.removeItem('token');
+  }
+
+  static getToken() {
+    return localStorage.getItem('token');
+  }
+}
+
+export default Auth;

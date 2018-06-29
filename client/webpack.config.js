afaf4a3d995a67ff454c6d2909b7f796a70c2398
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 12:35:49 +0200

    Add webpack config

---
+++ client/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = {
+  DIST: path.resolve(__dirname, '../server/public'),
+  JS: path.resolve(__dirname, './'),
+  SRC: path.resolve(__dirname, '.')
+};
+
+module.exports = {
+  entry: path.join(paths.JS, 'index.jsx'),
+  mode: 'development',
+  output: {
+    path: paths.DIST,
+    filename: 'app.js'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html')
+    })
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true
+  },
+  module: {
+    // apply rules to files that meet given conditions
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, '/'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: [
+            'babel-plugin-transform-class-properties',
+            'transform-object-rest-spread'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+
+  // start Webpack in a watch mode, so Webpack will rebuild the bundle on changes
+  watch: true,
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};

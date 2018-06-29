ff60dfc40e0da7d2c22e160de53afe9be666d1c8
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 14:38:39 +0200

    Add Text Component

---
+++ client/views/common/Text.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import fonts from '../constants/fonts';
+import colors from '../constants/colors';
+
+/* eslint-disable react/destructuring-assignment */
+const Text = styled.p`
+  font-family: ${fonts.primary};
+  font-size: ${props => (props.small ? '12px' : '14px')};
+  display: ${props => (props.inline ? 'inline' : 'block')};
+  color: ${props => (props.error ? colors.error : 'rgba(0, 0, 0, 0.65)')};
+`;
+/* eslint-enable react/destructuring-assignment */
+
+export default Text;

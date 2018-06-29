66c197a6f80c7df291ede78faccba757d7bc652f
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-29 14:56:30 +0200

    Add Layout component

---
+++ client/views/common/Layout.jsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import colors from '../constants/colors';
+
+/* eslint-disable react/destructuring-assignment */
+const Layout = styled.div`
+  ${props =>
+    props.sidebar
+      ? `
+    background: ${colors.background};
+    min-height: 100vh;
+    flex-direction: row;
+    box-sizing: border-box;
+    flex: auto;
+    display: flex;
+    `
+      : `
+    background: ${colors.background};
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: auto;
+    overflow-x: hidden;
+      `};
+`;
+/* eslint-enable react/destructuring-assignment */
+
+export default Layout;

4cfbae03dc09d1494c3daae831cd8c610ef83796
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-04 19:52:36 +0200

    Revert routes changes

--- client/views/routes/index.jsx
+++ client/views/routes/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import RegisterPage from '../pages/RegisterPage';
@@ -9,32 +7,20 @@ import DashboardTickets from '../pages/DashboardTickets';
 import LoginPage from '../pages/LoginPage';
 import LogoutPageContainer from '../containers/LogoutPageContainer';
 import DashboardSponsors from '../pages/DashboardSponsors';
-import { getUser } from '../../state/user/selectors';
-
-const propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired
-};
 
 /**
  * Routes component, return frontend routes
  */
-const Routes = ({ isLoggedIn }) => (
+const Routes = () => (
   <Switch>
     <Route exact path="/" component={HomePage} />
     <Route path="/register" component={RegisterPage} />
     <Route path="/login" component={LoginPage} />
-    <Route exact path="/dashboard" component={isLoggedIn ? DashboardHome : HomePage} />
-    <Route path="/logout" component={isLoggedIn ? LogoutPageContainer : HomePage} />
-    <Route path="/dashboard/tickets" component={isLoggedIn ? DashboardTickets : HomePage} />
-    <Route path="/dashboard/sponsors" component={isLoggedIn ? DashboardSponsors : HomePage} />
+    <Route exact path="/dashboard" component={DashboardHome} />
+    <Route path="/logout" component={LogoutPageContainer} />
+    <Route path="/dashboard/tickets" component={DashboardTickets} />
+    <Route path="/dashboard/sponsors" component={DashboardSponsors} />
   </Switch>
 );
 
-Routes.propTypes = propTypes;
-
-const mapStateToProps = state => ({
-  isLoggedIn: getUser(state).isLoggedIn
-});
-/* eslint-enable require-jsdoc */
-
-export default connect(mapStateToProps)(Routes);
+export default Routes;

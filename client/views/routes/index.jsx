f36b17f73f11b1ab344fa25c779ce50482b95da3
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-04 19:35:29 +0200

    Refactor Routes (Close #41)

--- client/views/routes/index.jsx
+++ client/views/routes/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import RegisterPage from '../pages/RegisterPage';
@@ -7,31 +9,32 @@ import DashboardTickets from '../pages/DashboardTickets';
 import LoginPage from '../pages/LoginPage';
 import LogoutPageContainer from '../containers/LogoutPageContainer';
 import DashboardSponsors from '../pages/DashboardSponsors';
+import { getUser } from '../../state/user/selectors';
 
-/* eslint-disable react/no-multi-comp */
-
-/**
- * Admin Routes
- */
-const AdminRoutes = () => (
-  <Switch>
-    <Route exact path="/dashboard" component={DashboardHome} />
-    <Route path="/logout" component={LogoutPageContainer} />
-    <Route path="/dashboard/tickets" component={DashboardTickets} />
-    <Route path="/dashboard/sponsors" component={DashboardSponsors} />
-  </Switch>
-);
+const propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
 
 /**
  * Routes component, return frontend routes
  */
-const Routes = () => (
+const Routes = ({ isLoggedIn }) => (
   <Switch>
     <Route exact path="/" component={HomePage} />
     <Route path="/register" component={RegisterPage} />
     <Route path="/login" component={LoginPage} />
-    <AdminRoutes />
+    <Route exact path="/dashboard" component={isLoggedIn ? DashboardHome : HomePage} />
+    <Route path="/logout" component={isLoggedIn ? LogoutPageContainer : HomePage} />
+    <Route path="/dashboard/tickets" component={isLoggedIn ? DashboardTickets : HomePage} />
+    <Route path="/dashboard/sponsors" component={isLoggedIn ? DashboardSponsors : HomePage} />
   </Switch>
 );
 
-export default Routes;
+Routes.propTypes = propTypes;
+
+const mapStateToProps = state => ({
+  isLoggedIn: getUser(state).isLoggedIn
+});
+/* eslint-enable require-jsdoc */
+
+export default connect(mapStateToProps)(Routes);

22290256d0a0badda9b07c1f20ae6553c639886b
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-07 00:22:57 +0200

    Rebase with develop

--- client/views/pages/AccountActivatedPage.jsx
+++ client/views/pages/AccountActivatedPage.jsx
@@ -1,15 +1,14 @@
 import React, { Fragment } from 'react';
-import Title from '../common/Title';
-import Button from '../common/Button';
+import Title from '../components/Title';
+import Button from '../components/Button';
 
 /**
  * * AccountActivatedPage for the root (/user/activated) route
  */
 const AccountActivatedPage = () => (
-    <Fragment>
-        <Title>Your account is now activated!</Title>
-        <Button>Continue</Button>
-    </Fragment>
-   
+  <Fragment>
+    <Title>Your account is now activated!</Title>
+    <Button>Continue</Button>
+  </Fragment>
 );
 export default AccountActivatedPage;

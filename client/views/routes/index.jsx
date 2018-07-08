604f9929c2d87aac5f5e81495dd2ef316f0905e5
Johannes <wiktorsson90@gmail.com>
2018-07-08 23:09:45 +0200

    tjo brush

--- client/views/routes/index.jsx
+++ client/views/routes/index.jsx
@@ -9,6 +9,7 @@ import LogoutPageContainer from '../containers/LogoutPageContainer';
 import DashboardSponsors from '../pages/DashboardSponsors';
 import DashboardTournaments from '../pages/DashboardTournaments';
 import AccountActivatedPage from '../pages/AccountActivatedPage';
+import ForgotPasswordPage from '../pages/ForgotPasswordPage';
 
 /**
  * Routes component, return frontend routes
@@ -24,6 +25,7 @@ const Routes = () => (
     <Route path="/dashboard/tournaments" component={DashboardTournaments} />
     <Route path="/dashboard/sponsors" component={DashboardSponsors} />
     <Route path="/user/activated" component={AccountActivatedPage} />
+    <Route path="/ForgotPasswordPage" component={ForgotPasswordPage} />
   </Switch>
 );
 

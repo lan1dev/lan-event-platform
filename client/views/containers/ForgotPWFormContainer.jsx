d89a38dc03f2075429292e97ace59ee1944857c3
Johannes <wiktorsson90@gmail.com>
2018-07-08 23:23:39 +0200

    amenvist

---
+++ client/views/containers/ForgotPWFormContainer.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import LoginForm from '../forms/ForgotPWForm';
+import { sessionLoginRequest } from '../../state/session/actions';
+import { getLoginErrors } from '../../state/session/selectors';
+import { getUser } from '../../state/user/selectors';
+
+/* eslint-disable require-jsdoc */
+const mapDispatchToProps = dispatch => ({
+  sendForm: (url, fields) => dispatch(sessionLoginRequest(url, fields))
+});
+
+const mapStateToProps = state => ({
+  isLoggedIn: getUser(state).isLoggedIn,
+  errors: getLoginErrors(state)
+});
+/* eslint-enable require-jsdoc */
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);

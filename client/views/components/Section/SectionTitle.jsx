a2e6a5a83c35accba4002182f30c486a893505c5
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-07-08 23:16:41 +0200

    Add SectionTitle Component

---
+++ client/views/components/Section/SectionTitle.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import fonts from '../../constants/fonts';
+
+const SectionTitle = styled.h2`
+  font-family: ${fonts.primary};
+  font-size: 28px;
+`;
+
+export default SectionTitle;

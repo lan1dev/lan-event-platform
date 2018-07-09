c27b54a203d5f8d658f8dfb3a917eae64094e7cd
Unknown <jespersundin8@gmail.com>
2018-07-09 22:03:23 +0200

    Add LANSection component (Close #35)

---
+++ client/views/components/LANSection/index.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Section from '../Section';
+import FlexBox from '../FlexBox';
+
+const LANSection = () => (
+  <Section>
+    <h1>Upcoming LAN events</h1>
+    <FlexBox direction="row" align="center" height="50%">
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/300/200" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+    </FlexBox>
+    <FlexBox direction="row" align="center" height="50%">
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+      <div>
+        <img hspace="20" alt="LAN" src="http://lorempixel.com/200/100" />
+        <p>blalalala</p>
+      </div>
+    </FlexBox>
+  </Section>
+);
+
+export default LANSection;
